a7f1104586f4515333ab9eaa38fb31b5e2471a44
JeremyRos08 <jeremyros08@gmail.com>
2026-09-15 21:40:44 +0200

    feat: mount microcontroller firmware windows

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './model/catalog-ext'
 import './ui/chrome/instrument-hud-drag'
 import App from './App'
+import { Esp32FirmwareOverlay } from './ui/chrome/Esp32FirmwareOverlay'
+import { ArduinoFirmwareOverlay } from './ui/chrome/ArduinoFirmwareOverlay'
 import './ui/styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <>
+      <App />
+      <Esp32FirmwareOverlay />
+      <ArduinoFirmwareOverlay />
+    </>
   </React.StrictMode>,
 )
